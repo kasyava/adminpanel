45a02f481c345dce7845d51f2fdb098720c3b741
syava <budo_slava@mail.ru>
2022-01-21 11:19:58 +0600

    Реализовал вывод истории транзакций

---
+++ src/components/History/HistoryList.js
@@ -0,0 +1,32 @@
+import React from "react";
+import {
+    List,
+    Datagrid,
+    TextField,
+    EmailField,
+    NumberField,
+    DateField,
+    Pagination
+} from 'react-admin';
+
+
+const MyPagination = props => <Pagination rowsPerPageOptions={[25, 50, 100]} {...props} />;
+
+
+export const HistoryList = props => (
+    <List {...props}
+          exporter={false}
+          bulkActionButtons={false}
+          pagination={<MyPagination/>}
+          perPage={25}
+
+    >
+        <Datagrid>
+            <EmailField source="email"/>
+            <TextField source="username"/>
+            <NumberField source="amount"/>
+            <TextField source="direction"/>
+            <DateField showTime source="date"/>
+        </Datagrid>
+    </List>
+);
