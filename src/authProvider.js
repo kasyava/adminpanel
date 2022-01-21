5574e02e8bec75af653fd3f26f496d6f132a85d3
syava <budo_slava@mail.ru>
2022-01-21 11:19:25 +0600

    Реализовал авторизацию через API посредством передачи логина и пароля и получения в ответ JWT токенов и уровня доступа.

--- src/authProvider.js
+++ src/authProvider.js
@@ -1,29 +1,56 @@
-export default {
+import decodeJwt from 'jwt-decode';
+
+
+const authProvider = {
     // called when the user attempts to log in
-    login: ({username}) => {
-        localStorage.setItem('username', username);
-        // accept all username/password combinations
-        return Promise.resolve();
+    login: ({username, password}) => {
+        const request = new Request(`${process.env.REACT_APP_API_URL}/admin/authenticate`, {
+            method: 'POST',
+            body: JSON.stringify({username, password}),
+            headers: new Headers({'Content-Type': 'application/json'}),
+        });
+        return fetch(request)
+            .then(response => {
+                if (response.status < 200 || response.status >= 300) {
+                    throw new Error(response.statusText);
+                }
+                return response.json();
+            })
+            .then(({token, refreshToken}) => {
+                const decodedToken = decodeJwt(token);
+                localStorage.setItem('token', token);
+                localStorage.setItem('refreshToken', refreshToken);
+                localStorage.setItem('permissions', decodedToken.permission);
+            });
     },
     // called when the user clicks on the logout button
     logout: () => {
-        localStorage.removeItem('username');
+        localStorage.removeItem('token');
+        localStorage.removeItem('refreshToken');
+        localStorage.removeItem('permissions');
         return Promise.resolve();
     },
     // called when the API returns an error
     checkError: ({status}) => {
         if (status === 401 || status === 403) {
-            localStorage.removeItem('username');
+            localStorage.removeItem('token');
+            localStorage.removeItem('refreshToken');
+            localStorage.removeItem('permissions');
             return Promise.reject();
         }
         return Promise.resolve();
     },
     // called when the user navigates to a new location, to check for authentication
     checkAuth: () => {
-        return localStorage.getItem('username')
+        return localStorage.getItem('token')
             ? Promise.resolve()
             : Promise.reject();
     },
     // called when the user navigates to a new location, to check for permissions / roles
-    getPermissions: () => Promise.resolve(),
+    getPermissions: () => {
+        const role = localStorage.getItem('permissions');
+        return role ? Promise.resolve(role) : Promise.reject();
+    }
 };
+
+export default authProvider;
